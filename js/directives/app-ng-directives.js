c276a90beff37aecfc054cdd38e9780f81c73461
huasu <huaofscience@gmail.com>
2015-04-13 06:13:09 -0400

    added new files

--- js/directives/app-ng-directives.js
+++ js/directives/app-ng-directives.js
@@ -22,53 +22,7 @@ angular.module('urlCheckApp.directives',[])
         });
     };
 })
-/**
- * Section for searching the Open Movie Database for movies
- */
-.directive('searchOmdb', function(){
-  return {
-    restrict: 'AE',
-    scope: {
-      title: '@',
-      url: '@',
-    },
 
-    templateUrl: 'templates/searchByQuery.html',
-    controller: function($scope, $element, urlFactory){
-      $scope.hideResults = false;
-      $scope.url = '';
-      $scope.movie = '';
-      $scope.searched = false;
-      $scope.baseurl = 'http://www.omdbapi.com/?';
-      $scope.type = 'JSONP';
-
-      $scope.updateURL = function(){
-        if ($scope.movie.length === 0) {
-          this.url = '';
-        }
-        var title = this.movie.split(' ').join('+');
-        this.url  = this.baseurl + 't=' + title + '&r=json&callback=JSON_CALLBACK';
-      };
-
-      $scope.search = function(){
-        if ($scope.movie.length == 0) {
-          return null;
-        }
-        $scope.searched = true;
-        // var now = new Date();
-        // console.log("Search pressed: "+now);
-
-        urlFactory.callURL(this.url)
-          .then(function(data){
-            $scope.data = data
-          }, function(data){
-            alert(data)
-          })
-      }
-
-    }
-  }
-})
 /**
  * Generic retreival of URLs
  * if the http method is jsonp, the callback will be added.
@@ -111,6 +65,104 @@ angular.module('urlCheckApp.directives',[])
         // console.log("Search pressed: "+now);
 
         urlFactory.callURL($scope.finalUrl)
+          .then(function(data){
+            $scope.data = data
+          }, function(data){
+            alert(data)
+          })
+      }
+
+    }
+  }
+})
+/**
+ * Section for searching the Open Movie Database for movies
+ */
+.directive('searchOmdb', function(){
+  return {
+    restrict: 'AE',
+    scope: {
+      title: '@',
+      url: '@',
+      queryname: '@'
+    },
+
+    templateUrl: 'templates/searchByQuery.html',
+    controller: function($scope, $element, urlFactory){
+      $scope.hideResults = false;
+      $scope.url = '';
+      $scope.query = '';
+      $scope.searched = false;
+      $scope.baseurl = 'http://www.omdbapi.com/?';
+      $scope.type = 'JSONP';
+
+      $scope.updateURL = function(){
+        if ($scope.query.length === 0) {
+          this.url = '';
+        }
+        var title = this.query.split(' ').join('+');
+        this.url  = this.baseurl + 't=' + title + '&r=json&callback=JSON_CALLBACK';
+      };
+
+      $scope.search = function(){
+        if ($scope.query.length == 0) {
+          return null;
+        }
+        $scope.searched = true;
+        // var now = new Date();
+        // console.log("Search pressed: "+now);
+
+        urlFactory.callURL(this.url)
+          .then(function(data){
+            $scope.data = data
+          }, function(data){
+            alert(data)
+          })
+      }
+
+    }
+  }
+})
+/**
+ * Section for searching the Open Movie Database for movies
+ * http://www.broadbandmap.gov/developer/api/demographics-api-by-geography-type-and-geography-name
+ * http://www.broadbandmap.gov/broadbandmap/demographic/jun2014/county/names/montgomery?format=jsonp&callback=JSON_CALLBACK
+ */
+.directive('searchNatbrmap', function(){
+  return {
+    restrict: 'AE',
+    scope: {
+      title: '@',
+      url: '@',
+      queryname: '@'
+    },
+
+    templateUrl: 'templates/searchByQuery.html',
+    controller: function($scope, $element, urlFactory){
+      $scope.hideResults = false;
+      $scope.url = '';
+      $scope.query = '';
+      $scope.searched = false;
+      $scope.baseurl = 'http://www.broadbandmap.gov/broadbandmap/demographic/jun2014/county/names/';
+      $scope.type = 'JSONP';
+
+      $scope.updateURL = function(){
+        if ($scope.query.length === 0) {
+          this.url = '';
+        }
+        var title = this.query.split(' ').join('+');
+        this.url  = this.baseurl + this.query + '?format=jsonp&callback=JSON_CALLBACK';
+      };
+
+      $scope.search = function(){
+        if ($scope.query.length == 0) {
+          return null;
+        }
+        $scope.searched = true;
+        // var now = new Date();
+        // console.log("Search pressed: "+now);
+
+        urlFactory.callURL(this.url)
           .then(function(data){
             $scope.data = data
           }, function(data){
